82b5a1c5fd0e1947481d38c000092a405c58a5d7
Chenx729 <1911926047@qq.com>
2022-04-12 15:06:43 +0800

    go操作MySQL Insert

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type Person struct {
+	UserId   int    `db:"user_id"`
+	Username string `db:"username"`
+	Sex      string `db:"sex"`
+	Email    string `db:"email"`
+}
+
+type Place struct {
+	Country string `db:"country"`
+	City    string `db:"city"`
+	TelCode int    `db:"telcode"`
+}
+
+var Db *sqlx.DB
+
+func init() {
+	db, err := sqlx.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test_db")
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+	Db = db
+}
 
 func main() {
-	fmt.Println("hello world")
+	r, err := Db.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+	defer Db.Close() // 注意这行代码要写在上面err判断的下面
+	id, err := r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+
+	fmt.Println("insert succ:", id)
 }
